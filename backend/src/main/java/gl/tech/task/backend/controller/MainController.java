cc4aefa75d8f2e70dfc723ccbe2be36acf1f0a04
Vestars <nestly093>
2019-09-10 04:14:04 +0300

    add @Validated annotation to rest controller

--- backend/src/main/java/gl/tech/task/backend/controller/MainController.java
+++ backend/src/main/java/gl/tech/task/backend/controller/MainController.java
@@ -6,10 +6,13 @@ import gl.tech.task.backend.utility.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
+@Validated
 @RestController
 @RequestMapping("/api")
 public class MainController {
